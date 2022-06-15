a461ffe9c4e53b102ee00a3be5c14567ee9b7384
ArjanMeli <arjanmeli23@gmail.com>
2022-06-15 23:17:00 +0200

    initial commit

---
+++ src/users.js
@@ -0,0 +1,51 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+
+
+
+
+export const Users = [
+
+    {
+        id:1,
+        first_name: "Aguliqja",
+        img: img1,
+        paragraf: "Për të 12 vit me radhë Aguliqja e ruan primatin ndër produktet tona më të dashura! E para e pranveres Aguliqja tek ne përpunohet çaj me vlera të larta shëndetësore dhe serum hidratues për të ruajtur fytyrën rinore! Aguliqe-Primula veris nga zona e çertifikuar organike.",
+        
+        
+        
+
+    },
+
+    {
+        id:2,
+        first_name: "Dëllinja",
+        img: img2,
+        paragraf: "Dëllinja është bimë shumë shëruese e cila rritet në zonat shkëmbore dhe karstike, nga zonat bregdetare e deri tek ato malore. Frutat e saj më së shumti përdoren te problemet me stomak dhe problemet me kanalet urinare dhe shihet si një nga diuretikët më të mirë natyrorë.",
+
+        
+        
+        
+
+    },
+
+    {
+        id:3,
+        first_name: "Mjalti shumëlulesh",
+        img: img3,
+        paragraf: "Mjalti shumëlulesh vjen nga Kukesi, zona e Shkinakut. Eshte mjaltë i ri dhe me cilësi të lartë, pasi kullota e bletëve është ndër më të pasurat. Ky mjaltë ka marrë vleresime si brenda edhe jashtë vendit.",
+        
+
+    },
+
+    {
+        id:4,
+        first_name: "Boronicat",
+        img: img4,
+        paragraf: "Boronicat janë të ëmbla, ushqyese dhe shumë të shëndetshme. Këto fruta kaq të vogla i bëjnë kaq mirë trupit dhe mendjes sonë. Boronica ndihmon trurin dhe zemrën, mbron sytë dhe lëkurën.", 
+
+    },
+
+]
